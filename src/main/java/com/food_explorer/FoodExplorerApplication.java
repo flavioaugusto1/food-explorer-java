d2bb575a996eb53b4898f3f8e7bb89d686aa3fe7
Flávio <f.augustosdn@gmail.com>
2025-12-21 19:13:51 -0300

    iniciando projeto

---
+++ src/main/java/com/food_explorer/FoodExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.food_explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/food_explorer/FoodExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
